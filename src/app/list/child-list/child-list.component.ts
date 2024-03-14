d71496a8407c025f84c540fc7c0f3e3da1856748
KKoaLLa04 <ndkdzvl@gmail.com>
2024-03-15 01:06:07 +0700

    commit

--- src/app/list/child-list/child-list.component.ts
+++ src/app/list/child-list/child-list.component.ts
@@ -13,8 +13,8 @@ export class ChildListComponent {
   @Input() data: any = '';
 
   modalRef?: BsModalRef;
-
   username: FormGroup;
+  message?: string;
 
   constructor(
     private userService: UserService,
@@ -23,14 +23,14 @@ export class ChildListComponent {
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     ){
-      // pagination
       this.username = this.formBuilder.group({
         avatar: ['', Validators.required],
         name: ['', Validators.required],
         username: ['', Validators.required],
         gender: ['', Validators.required],
         date_of_study: ['', Validators.required],
-        block: [[false, false], Validators.required],
+        block1: [false],
+        block2: [false],
         status: ['', Validators.required],
         password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20) , Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()/.=+]).{6,20}")]],
         age: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
@@ -38,23 +38,14 @@ export class ChildListComponent {
       });
     }
 
-  deleteUser(id: number){
-    let checkSure = confirm("Ban co chac chan muon xoa?");
-
-    if(checkSure){
-      this.userService.deleteUser(id).subscribe(data => {
-        alert("Xoa nguoi dung thanh cong");
-        this.router.navigateByUrl('/list-component');
-      })
-    }
-  }
-
+  // handle search
   searchText:String = '';
 
   onFilterSearchText(value: string){
     this.searchText = value;
   }
 
+  // handle pagination
   currentPageData = 1;
   perPage: number = 5;
 
@@ -76,31 +67,46 @@ export class ChildListComponent {
   }
 
   
+  // checkbox block
+  blockArr = [
+    {id:1,name:"Kỳ 1", isSelected: true},
+    {id:2,name:"Kỳ 2", isSelected: false},
+    {id:3,name:"Kỳ 3", isSelected: false},
+  ];
+
+  onChange(event: any){
+    console.log(event.target.value);
+  }
 
   // modal
   openModal(template: TemplateRef<void>) {
     this.modalRef = this.modalService.show(template);
   }
 
-  // edit
-  // initForm(data: any){
-  //   this.username = this.formBuilder.group({
-  //     avatar: [data.avatar, Validators.required],
-  //     name: [data.name, Validators.required],
-  //     username: [data.username, Validators.required],
-  //     gender: [data.gender, Validators.required],
-  //   });
-  // }
-
-  // hem moi
+  // add
   changeDataToParent(){
-    let dataRequest = {
+    // Block fix cứng tạm thời
+    interface UserDataRequest {
+      avatar: string;
+      name: string;
+      username: string;
+      gender: string;
+      date_of_study: string;
+      block1: boolean;
+      block2: boolean;
+      status: string;
+      password: string;
+      age: number;
+      date: string;
+    }
+    let dataRequest: UserDataRequest = {
       avatar: this.username.value.avatar,
       name: this.username.value.name,
       username: this.username.value.username,
       gender: this.username.value.gender,
       date_of_study: this.username.value.date_of_study,
-      block: this.username.value.block,
+      block1: this.username.value.block1,
+      block2: this.username.value.block2,
       status: this.username.value.status,
       password: this.username.value.password,
       age: this.username.value.age,
@@ -112,14 +118,12 @@ export class ChildListComponent {
 
   submitForm(){
     if(this.username.valid){
-      // add
       let data = this.changeDataToParent();
       this.userService.addUser(data).subscribe(response => {
         alert("Thêm sản phẩm thành công!");
         window.location.reload();
       })
     }else{
-      console.log(this.username);
       this.markFormGroupAsTouched(this.username);
     }
   }
@@ -131,35 +135,45 @@ export class ChildListComponent {
   }
 
   // show status list
-
   // edit
-  // username: any;
   userData: any;
   idEdit: number = 0;
 
   openEditModal(template: TemplateRef<void>, itemId:number) {
+    interface UserData {
+      avatar: string;
+      name: string;
+      username: string;
+      gender: string;
+      date_of_study: string;
+      block: boolean[];
+      status: string;
+      password: string;
+      age: number;
+      date: string;
+    }
+    
     this.modalRef = this.modalService.show(template);
     this.idEdit = itemId;
     this.getDetailUser(itemId);
-    console.log(this.getDetailUser(itemId));
   }
 
   initFormEdit(data: any){
     this.username = this.formBuilder.group({
       avatar: [data.avatar, Validators.required],
-        name: [data.name, Validators.required],
-        username: [data.username, Validators.required],
-        gender: [data.gender, Validators.required],
-        date_of_study: [data.date_of_study, Validators.required],
-        block: [data.block, Validators.required],
-        status: [data.status, Validators.required],
-        password: [data.password, [Validators.required, Validators.minLength(6), Validators.maxLength(20) , Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()/.=+]).{6,20}")]],
-        age: [data.age, [Validators.required, Validators.min(1), Validators.max(100)]],
-        date: [data.date, Validators.required],
+      name: [data.name, Validators.required],
+      username: [data.username, Validators.required],
+      gender: [data.gender, Validators.required],
+      date_of_study: [data.date_of_study, Validators.required],
+      block: [data.block, Validators.required],
+      status: [data.status, Validators.required],
+      password: [data.password, [Validators.required, Validators.minLength(6), Validators.maxLength(20) , Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()/.=+]).{6,20}")]],
+      age: [data.age, [Validators.required, Validators.min(1), Validators.max(100)]],
+      date: [data.date, Validators.required],
     });
   }
 
-  getDetailUser(idItem: any){
+  getDetailUser(idItem: number){
     this.userService.getDetailUser(idItem).subscribe(data => {
       this.userData = data;
       console.log(this.userData);
@@ -172,11 +186,30 @@ export class ChildListComponent {
       // add
       let data = this.changeDataToParent();
       this.userService.updateUser(this.idEdit, data).subscribe(response => {
-        alert("Thêm sản phẩm thành công!");
+        alert("Cập nhật sản phẩm thành công!");
         window.location.reload();
       })
     }else{
       this.markFormGroupAsTouched(this.username);
     }
+  }
+
+  // delete
+  openModalDetete(template: TemplateRef<void>) {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+  }
+ 
+  confirm(id: number): void {
+    // this.message = 'Confirmed!';
+    // this.modalRef?.hide();
+    this.userService.deleteUser(id).subscribe(data => {
+      alert("Xoa nguoi dung thanh cong");
+      window.location.reload();
+    })
+  }
+ 
+  decline(): void {
+    this.message = 'Declined!';
+    this.modalRef?.hide();
   }
 }
